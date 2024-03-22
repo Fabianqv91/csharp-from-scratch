3a1c360e5b1b6b6fd13893f60b79658dd5aa6e4e
Fabian <fabian.9105@gmail.com>
2024-03-22 16:57:25 -0500

    Polimorfismos

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,18 @@
+﻿/*conditionals*/
+// Console.WriteLine($"Ingrese un numero: ");
+// int num = int.Parse(Console.ReadLine());
+// if(num > 0) Console.WriteLine("Positivo");
+// else if(num < 0) Console.WriteLine("Negativo");
+// else Console.WriteLine("Es cero");
+
+/*end conditionls*/
+
+/*Operators*/
+Console.WriteLine("Ingrese el primer numero: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
+Console.WriteLine($"suma: {a+b}, Resta: {a-b}, Multiplicacion {a*b}, Division {(b!=0 ? a/b : Indeterminado)}");
+
+/*End operators*/
